ed79d42773d40f47352a7972fc2d67881eacce44
Kevin Peng <kevinpkf311@gmail.com>
2021-03-24 18:13:31 -0700

    Added .env to gitignore and added tags+contributors attirbute to models

--- server/models/snapshot.model.js
+++ server/models/snapshot.model.js
@@ -19,6 +19,8 @@ const snapshotSchema = new Schema(
       type: String,
       required: true,
     },
+    tags: [{ type: String }],
+    contributors: [{ type: String }],
   },
   {
     timestamps: true,
