f01f71defcb9e44a6345cdbb6f8680e01d30ebde
Tiffany <tiffwu1027@gmail.com>
2021-04-10 20:49:56 -0700

    added delete snapshot by id endpoint

--- server/router.js
+++ server/router.js
@@ -9,4 +9,7 @@ router.post("/api/snapshots", Controller.createSnapshot);
 // Define GET route for getting all timeline snapshots
 router.get("/api/snapshots", Controller.getAllSnapshots);
 
+// Define DEL route for deleting a snapshot given the ID
+router.delete("/api/snapshots/:id", Controller.deleteSnapshot);
+
 module.exports = router;
