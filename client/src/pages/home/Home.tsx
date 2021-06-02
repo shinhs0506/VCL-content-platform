8036ee7807f8e80e92bf3bd115ea2a3fe5c866ad
Kevin Peng <kevinpkf@gmail.com>
2021-06-01 19:49:12 -0700

    Fixed button generic

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Button from "@components/generics/Button";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = (props) => {
-  return <div>HI THIS IS THE LANDING PAGE</div>;
+  return (
+    <div>
+      <div>HI THIS IS THE LANDING PAGE</div>
+      <Button>TESTING</Button>
+    </div>
+  );
 };
 
-export default Home
+export default Home;
