140a95256bc0aa817f5787540ab7628031dacc89
Vaibhav <vaibhav@Vaibhavs-MacBook-Pro.local>
2021-07-20 09:54:31 -0700

    Refactored navigation rendering to iterate over list

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 //TODO: Refactor navbar to generic
 interface NavbarProps {}
 
+const links = [
+  {
+    ref: '/',
+    text: 'Home'
+  },
+  {
+    ref: '/timeline',
+    text: 'Timeline'
+  },
+  {
+    ref: '/projects',
+    text: 'Projects'
+  }
+]
+
 const Navbar: React.FC<NavbarProps> = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const renderedLinks = links.map((linkInfo, index) => {
+    return (
+      <React.Fragment key={linkInfo.ref}>
+        <li>
+          <Link className="nav-link" to={linkInfo.ref}>{linkInfo.text}</Link>
+        </li>
+      </React.Fragment>
+    )
+  })
+
   return (
     <div className="navbar-container">
       <p className="title">UBC Visual Cognition Lab</p>
       <nav>
         <ul className="nav-links">
-          <li>
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/timeline">Timeline</Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/projects">Projects</Link>
-          </li>
+          {renderedLinks}
         </ul>
       </nav>
       <button>Search</button>
