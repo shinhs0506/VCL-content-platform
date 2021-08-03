71af15f4bc6aa18109307c6e0a607e455effc5fb
sallylim <lim.sally20@gmail.com>
2021-08-03 11:10:05 -0700

    Added classnames and basic css to css file.

--- src/components/generics/TimelineCommitBlock/TimelineCommitBlock.tsx
+++ src/components/generics/TimelineCommitBlock/TimelineCommitBlock.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import moment from "moment";
 import Chip from '@material-ui/core/Chip';
-
+import './TimelineCommitBlock.css';
 interface TimelineCommitBlockProps {
     author: string;
     elementChanged: string;
@@ -15,7 +15,7 @@ const TimelineCommitBlock: React.FC<TimelineCommitBlockProps> = (props) => {
     const [expand, setExpand] = useState(false);
 
     return(
-        <div style={{textAlign:"left"}}
+        <div className="text"
              onClick={() => setExpand(!expand)}>
             { expand ? ExpandedTimelineContent(props): ClosedTimelineContent(props) }
         </div>
@@ -26,18 +26,17 @@ const ExpandedTimelineContent: React.FC<TimelineCommitBlockProps> = (props) => {
     const { author, elementChanged, date, project,description, tags } = props;
 
     return(
-        <div style={{textAlign:"left"}}> {/** Basic styling, move to css file **/}
-            <p>{author} added {elementChanged} to {project}</p>
-            <p>{moment(date).format('MMMM DD, YYYY')}</p>
-            <div> 
-            {/** style={{display:"flex", flexWrap:"wrap", flexDirection:"row"}} **/}
+        <div className="expandedTimeline">
+            <p className="header">{author} added {elementChanged} to {project}</p>
+            <p className="date">{moment(date).format('MMMM DD, YYYY')}</p>
+            <div className="tag"> 
             {tags.map((tagName) => (
                 <Chip
                  label={tagName}
                 />
             ))}
             </div>
-            <p>{description}</p>
+            <p className="shortDescription">{description}</p>
         </div>
     );
 }
@@ -45,10 +44,10 @@ const ClosedTimelineContent: React.FC<TimelineCommitBlockProps> = (props) => {
     const { author, elementChanged, date, project,description, tags } = props;
 
     return(
-        <div style={{textAlign:"left"}}>
-            <p>{author} added {elementChanged} to {project}</p>
-            <p>{moment(date).format('MMMM DD, YYYY')}</p>
-            <p>{description}</p>
+        <div className="closedTimeline">
+            <p className="header">{author} added {elementChanged} to {project}</p>
+            <p className="date">{moment(date).format('MMMM DD, YYYY')}</p>
+            <p className="expandedDescription">{description}</p>
         </div>
     );
 }
