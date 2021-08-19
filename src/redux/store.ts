d5988842214aad6e1a88e1d1daab0e06bf06d19f
Kevin Peng <kevinpkf@gmail.com>
2021-08-19 11:55:55 -0700

    Added redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,31 @@
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
 
 // Generic reducer imports go here
 import accessControllerReducer from './generics/AccessControllerRedux';
 import authReducer from './generics/AuthRedux';
 // Component/Page reducer imports go here
+// ...
+
+// Persist config
+const createPersistConfig = (key: string, blacklist?: string[]) => ({
+  key,
+  storage,
+  blacklist,
+});
+
+const rootReducer = combineReducers({
+  accessController: persistReducer(createPersistConfig("accessController"), accessControllerReducer),
+  auth: persistReducer(createPersistConfig("auth"), authReducer),
+});
 
 export const store = configureStore({
-  reducer: {
-    accessController: accessControllerReducer,
-    auth: authReducer,
-  },
+  reducer: rootReducer,
 });
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
