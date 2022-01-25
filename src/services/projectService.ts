0ea6587ceb4ba0382fc9d92b3e78d2bfa72a645a
John Shin <shinhs0506@gmail.com>
2022-01-24 23:06:48 -0800

    set alert to handle api error

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -1,6 +1,7 @@
 import { useEffect }from 'react';
 import { useAppDispatch } from '@redux/hooks';
 import { projectActions } from '@redux/slices/ProjectRedux';
+import { appActions } from '@redux/slices/AppRedux';
 import { callGetAllProjectsAPI } from '@services/adapters/projectAdapter'
 
 export const useHandleGetAllProjects = () => {
@@ -11,6 +12,8 @@ export const useHandleGetAllProjects = () => {
             .then((res) => {
                 if (res.data) {
                     dispatch(projectActions.setProjects(res.data)) 
+                } else {
+                    dispatch(appActions.setAlert(res.message));
                 }
             })
             .catch(() => console.error('Error: projectService.ts getAllProjects call'))
