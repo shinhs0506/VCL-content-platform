47586de3646073eb3629009a93655810109197a4
John Shin <shinhs0506@gmail.com>
2022-04-25 12:53:42 -0700

    fixed alignment issue on small window size, able to toggle the sidebar

--- src/pages/Project/ProjectWrapper.tsx
+++ src/pages/Project/ProjectWrapper.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { RouteComponentProps, Switch, Route } from 'react-router';
 import { useAppSelector } from '@redux/hooks';
 import { selectProjects } from '@redux/slices/ProjectRedux';
+import ToggleButton from '@mui/material/ToggleButton'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import ROUTES from '@statics/routes';
 import { 
 ProjectDefault, 
@@ -23,7 +26,7 @@ interface ProjectProps extends RouteComponentProps<MatchParams> {
 
 const ProjectWrapper: React.FC<ProjectProps> = ({match}) => {
     const [ selected, setSelected ] = React.useState(true);
-    const expand = selected? '' : 'expand'
+    const expand = selected? 'expand' : '';
     // TODO: add a button open/close the sidebar
 
     const projects = useAppSelector(selectProjects); 
@@ -43,6 +46,21 @@ const ProjectWrapper: React.FC<ProjectProps> = ({match}) => {
 
     return (
         <div className={"project-page"}>
+            <div className={["sidebar", expand].join(' ')}>
+                <ProjectNavbar isOpen={selected} links={links}/>  
+            </div>
+            <div className="togglebutton"> 
+                <ToggleButton 
+                    className="togglebutton"
+                    value="check"
+                    selected={selected}
+                    onChange={() => {
+                        setSelected(!selected);
+                    }}
+                > 
+                {selected? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </ToggleButton>
+            </div>
             <div className={"content-container"}>
                 <Switch>
                     <Route exact path={match.url} render={() => <ProjectDefault project={curr_project}/>}/>
@@ -51,9 +69,6 @@ const ProjectWrapper: React.FC<ProjectProps> = ({match}) => {
                     <Route exact path={`${match.url}/team`} render={() => <ProjectTeam project={curr_project}/>}/>
                     <Route exact path={`${match.url}/contact`} render={() => <ProjectContact project={curr_project}/>}/>
                 </Switch>
-            </div>
-            <div className="sidebar">
-                <ProjectNavbar isOpen={selected} links={links}/>  
             </div>
         </div>
     )
